2ada6ce01b15dd9abd88ecca2d1d4c2c2d78f080
Ori Roll <oriroll@gmail.com>
2020-06-23 12:11:47 +0300

    stars selection

--- src/Components/Project/Tab/Task/Cells/StarsCell/StarsCell.js
+++ src/Components/Project/Tab/Task/Cells/StarsCell/StarsCell.js
@@ -1,20 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import PropTypes from "prop-types";
 import style from "./StarsCell.module.css";
 
 function StarsCell({ cellData = { content: 0 } }) {
-	const [starsOn, setStarsOn] = useState(3);
-
+	const [starsOnSet, setStarsOnSet] = useState(cellData.content);
+	const [starsOn, setStarsOn] = useState(cellData.content);
 	let allStars = new Array(5);
 
 	return (
 		<div className={style["star-cell"]}>
 			{allStars.fill(undefined).map((item, i) => {
-				return starsOn > i ? <div key={`${i}`}>&#9733;</div> : <div key={`${i}`}>&#9734;</div>;
+				return (
+					<div
+						key={`${i}`}
+						onMouseOver={() => setStarsOn(i + 1)}
+						onMouseLeave={() => setStarsOn(starsOnSet)}
+						onClick={() => setStarsOnSet(i + 1)}>
+						{starsOn > i ? <>&#9733;</> : <>&#9734;</>}
+					</div>
+				);
 			})}
 		</div>
 	);
-
+	/* `&#9733`; : `&#9734;` */
 	/* for (let i = 5; i > 0; i++) {
 			return starsOn >= i ? <div>&#9733;</div> : <div>&#9734;</div>;
 		} */
