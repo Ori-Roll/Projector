26db83ee1f5cb9d05096c1d3a6803954e3d3dd77
Ori Roll <oriroll@gmail.com>
2020-07-13 14:55:25 +0300

    Description cell works better

--- src/Components/Project/Tab/Task/Cells/DescriptionCell/DescriptionCell.js
+++ src/Components/Project/Tab/Task/Cells/DescriptionCell/DescriptionCell.js
@@ -3,36 +3,29 @@ import PropTypes from "prop-types";
 
 import { NEW_COLUMN_DATA } from "../../../../../defaults";
 
+import CellsStyle from "../CellsStyle.module.css";
 import style from "./DescriptionCell.module.css";
 
-function DescriptionCell({
-	cellData = { id: Math.random(), content: NEW_COLUMN_DATA["description"].newCellContent }, //TODO: remove Math-random!!!
-	task,
-	doCellContentChange,
-}) {
-	const [cellText, setCellText] = useState(cellData.content);
+function DescriptionCell({ id, content, doCellContentChange }) {
+	const [cellText, setCellText] = useState(content);
 
-	useEffect(() => {
-		/* dispatchChange(); */
-	}, [cellText]);
+	console.log(`%c ------cell render! content: ${content}---------`, "color: blue");
+	useEffect(() => console.log("%c TextCell MOUNT!", "color:red"), []);
 
-	function onInputChange(e) {
-		setCellText(e.target.value);
+	function onInputChange(text) {
+		setCellText(text);
+		doCellContentChange(id, text, true);
 	}
 
-	/* function dispatchChange() {
-		if (!cellText) return;
-		let editedTask = { ...task };
-		editedTask[cellData.id].content = cellText;
-		changeTabData({ editedTask: editedTask, type: "EDIT_TASK" });
+	function onBlur() {
+		doCellContentChange(id, cellText, false);
 	}
- */
 	return (
 		<input
-			className={style["description-cell-wrapper"]}
+			className={`${style["description-cell"]} ${CellsStyle["cell"]}`}
 			value={cellText}
-			onChange={(e) => onInputChange(e)}
-			/* onBlur={() => dispatchChange()} */
+			onChange={(e) => onInputChange(e.target.value)}
+			onBlur={() => onBlur()}
 		/>
 	);
 }
