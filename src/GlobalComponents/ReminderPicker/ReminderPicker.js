e45f48c1709937aa3adc8837018e740d945e0ccd
Ori Roll <oriroll@gmail.com>
2020-11-22 17:53:24 +0200

    starting to work on ReminderPicker

---
+++ src/GlobalComponents/ReminderPicker/ReminderPicker.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import style from "./ReminderPicker.module.css"
+
+function ReminderPicker(props) {
+    return (
+        <div className={style["picker-wrapper"]}>
+            
+        </div>
+    )
+}
+
+ReminderPicker.propTypes = {
+
+}
+
+export default ReminderPicker
+
