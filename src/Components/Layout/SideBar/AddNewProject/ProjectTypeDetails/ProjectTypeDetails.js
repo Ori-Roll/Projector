3b82365e6a5fa6ff37ad4777cd5d77556bdd3451
Ori Roll <oriroll@gmail.com>
2020-11-26 12:04:47 +0200

    loading message for new project

--- src/Components/Layout/SideBar/AddNewProject/ProjectTypeDetails/ProjectTypeDetails.js
+++ src/Components/Layout/SideBar/AddNewProject/ProjectTypeDetails/ProjectTypeDetails.js
@@ -4,21 +4,31 @@ import PropTypes from 'prop-types'
 import {useDispatch} from "react-redux"
 
 import { createNewProject } from '../../../../ServerProvider/projects';
-
-import {setProjectDispatch} from "../../../../redux/rootReducer"
+import { setProjectDispatch } from "../../../../redux/rootReducer";
+import useResetTemporaryOperations from "../../../../../GlobalComponents/globalControllers/useResetTemporaryOperations";
 
 import style from "./ProjectTypeDetails.module.css";
 
 function ProjectTypeDetails({selectedProjectType, setSelectedProjectType, setAddNewProjectMenuActive}) {
-    
-    const dispatch = useDispatch()
 
+    const dispatch = useDispatch()
     const setProjectToSelected = (project) => dispatch(setProjectDispatch(project))
+
+    const resetTemporaryOperations = useResetTemporaryOperations("all");
+
+    const loadingProjectMessage = 
+        <div className={style["loading-project-message"]}>
+            <p>Just a moment...</p>
+            <p>"Creating your new project."</p>
+            <img src={ `http://localhost:5000/api/v0/projectTypes/${selectedProjectType.icon}`}/>
+        </div>;
 
     async function onDoneClick(){
         setAddNewProjectMenuActive(false);
+        resetTemporaryOperations({projectLoadingMessage: loadingProjectMessage});
         const newProjectRes = await createNewProject(selectedProjectType.projectProperties);
         console.log("newProjectRes", newProjectRes);
+        debugger
         setProjectToSelected(newProjectRes);
     }
     
