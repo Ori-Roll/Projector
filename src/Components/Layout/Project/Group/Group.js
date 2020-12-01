804ee5d78c194babdbd2733cacb6eefe0d6fc450
Ori Roll <oriroll@gmail.com>
2020-12-01 13:12:56 +0200

    task/group add mock task WIP

--- src/Components/Layout/Project/Group/Group.js
+++ src/Components/Layout/Project/Group/Group.js
@@ -1,85 +1,93 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import {useSelector, useDispatch} from "react-redux";
-import _ from "lodash";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import _ from 'lodash';
 
-import {db_createNewTask} from "../../../../Components/ServerProvider/tasks"
-import {addTaskDispatch} from "../../../redux/rootReducer";
+import { db_createNewTask } from '../../../../Components/ServerProvider/tasks';
+import { addTaskDispatch } from '../../../redux/rootReducer';
 
-import Task from "./Task/Task";
-import TaskAddFooter from "./TaskAddFooter/TaskAddFooter";
-import ColumnsHeadWrapper from "./ColumnsHeadWrapper/ColumnsHeadWrapper";
-import GroupHeader from "./GroupHeader/GroupHeader";
-import Loader from "../../../../GlobalComponents/Loader/Loader"
+import Task from './Task/Task';
+import TaskAddFooter from './TaskAddFooter/TaskAddFooter';
+import ColumnsHeadWrapper from './ColumnsHeadWrapper/ColumnsHeadWrapper';
+import GroupHeader from './GroupHeader/GroupHeader';
+import Loader from '../../../../GlobalComponents/Loader/Loader';
 
-import style from "./Group.module.css"; // TODO: rename file to lowercase, in my projects it's always style(s).module.css
+import style from './Group.module.css'; // TODO: rename file to lowercase, in my projects it's always style(s).module.css
 
 function Group({ group, groupIndex }) {
-	const [groupIsOpen, setGroupIsOpen] = useState(true); // TODO: (Ori) this needs to initially come from backend
-	
-	const user = useSelector(state => state.user)
-	const dispatch = useDispatch();
-	const addTask = (task, groupIndex) => dispatch(addTaskDispatch(task, groupIndex));
+  const [groupIsOpen, setGroupIsOpen] = useState(true); // TODO: (Ori) this needs to initially come from backend
 
-	async function addNewTask() {
-		try {
-			const newTask = {
-				title: "-",
-				assignedTo: [user._id],
-				project: group.project, //TODO: better get this from global/store
-				group: group._id,
-				isMock: true
-			};
-			let task = await db_createNewTask(newTask);
-			task = task.data;
-			addTask(task, groupIndex);
-		} catch (error) {
-			console.error(error);
-		}
-	}
-	
-	useEffect(() => {
-		if( group.tasks &&
-			group.tasks[group.tasks.length-1] 
-			&& (group.tasks[group.tasks.length-1].isMock === false)){
-			addNewTask();
-		}
-	}, [])
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const addTask = (task, groupIndex) =>
+    dispatch(addTaskDispatch(task, groupIndex));
 
-	return (
-		/* TODO add classcat package:  */
-		<div className={style["group"]}>
-			<div className={style["group-header-wrapper"]}>
-				<GroupHeader
-					groupIsOpen={groupIsOpen}
-					setGroupIsOpen={setGroupIsOpen}
-					group={group}
-					groupIndex={groupIndex}
-				/>
-			</div>
-			<div
-				className={style["group-content-wrapper"]}
-				style={!groupIsOpen ? { backgroundColor: "red", display: "none" } : {}}>
-				<ColumnsHeadWrapper key={group._id} columns={group.columns} groupIndex={groupIndex} />
-				{group.loaded ? (
-					group.tasks.map((task, i) => {
-						return (
-							<Task
-								key={task._id}
-								task={task}
-								columns={group.columns}
-								taskIndex={i}
-								groupIndex={groupIndex}
-								addNewTask={addNewTask}
-							/>
-						);
-					})
-				) : (
-					<Loader message={"Loading group"}/>
-				)}
-			</div>
-		</div>
-	);
+  async function addNewTask() {
+    try {
+      const newTask = {
+        title: '-',
+        assignedTo: [user._id],
+        project: group.project, //TODO: better get this from global/store
+        group: group._id,
+        isMock: true,
+      };
+      let task = await db_createNewTask(newTask);
+      task = task.data;
+      addTask(task, groupIndex);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    if (
+      group.tasks &&
+      group.tasks[group.tasks.length - 1] &&
+      group.tasks[group.tasks.length - 1].isMock === false
+    ) {
+      addNewTask();
+    }
+  }, []);
+
+  return (
+    /* TODO add classcat package:  */
+    <div className={style['group']}>
+      <div className={style['group-header-wrapper']}>
+        <GroupHeader
+          groupIsOpen={groupIsOpen}
+          setGroupIsOpen={setGroupIsOpen}
+          group={group}
+          groupIndex={groupIndex}
+        />
+      </div>
+      <div
+        className={style['group-content-wrapper']}
+        style={!groupIsOpen ? { backgroundColor: 'red', display: 'none' } : {}}
+      >
+        <ColumnsHeadWrapper
+          key={group._id}
+          columns={group.columns}
+          groupIndex={groupIndex}
+        />
+        {group.loaded ? (
+          group.tasks.map((task, i) => {
+            return (
+              <Task
+                key={task._id}
+                task={task}
+                columns={group.columns}
+                taskIndex={i}
+                groupIndex={groupIndex}
+                addNewTask={addNewTask}
+              />
+            );
+          })
+        ) : (
+          <Loader message={'Loading group'} />
+        )}
+      </div>
+    </div>
+  );
 }
 
 Group.propTypes = {};
--- src/Components/Layout/Project/Group/Task/Task.js
+++ src/Components/Layout/Project/Group/Task/Task.js
@@ -1,126 +1,145 @@
-import React, { useState, useEffect, useRef } from "react";
-import PropTypes from "prop-types";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
-import _ from "lodash";
-import { useInView } from "react-intersection-observer";
+import _ from 'lodash';
+import { useInView } from 'react-intersection-observer';
 
-import { db_changeTask } from "../../../../ServerProvider/tasks";
-import { editTaskDispatch } from "../../../../redux/rootReducer";
+import { db_changeTask } from '../../../../ServerProvider/tasks';
+import { editTaskDispatch } from '../../../../redux/rootReducer';
 
-import { CellOfType } from "./Cells/CellTypes/CellTypes";
-import CellWrapper from "./CellWrapper";
+import { CellOfType } from './Cells/CellTypes/CellTypes';
+import CellWrapper from './CellWrapper';
 
-import style from "./Task.module.css";
-import SelectCheckbox from "./SelectCheckbox/SelectCheckbox";
+import style from './Task.module.css';
+import SelectCheckbox from './SelectCheckbox/SelectCheckbox';
 
-function Task({ task, columns, taskIndex, groupIndex, addNewTask}) {
-	
-	const [taskWrapperRef, inView, entry] = useInView();
+function Task({ task, columns, taskIndex, groupIndex, addNewTask }) {
+  const [taskWrapperRef, inView, entry] = useInView();
 
-	const [taskIsSelected, setTaskIsSelected] = useState(false);
+  const [taskIsSelected, setTaskIsSelected] = useState(false);
 
-	const dispatch = useDispatch();
-	const editTask = (groupIndex, taskIndex, task) =>
-		dispatch(editTaskDispatch(groupIndex, taskIndex, task));
+  const dispatch = useDispatch();
+  const editTask = (groupIndex, taskIndex, task) =>
+    dispatch(editTaskDispatch(groupIndex, taskIndex, task));
 
-	function createCell(column = () => console.error("no column for cell"), options = []) {
-		if (!column.type) throw new Error("No column.type for cell");
-		const cell = task.cells.find((cell) => cell.columnMatch === column._id);
-		if (!cell) {
-			console.error(`task with no cell match for column.id ${column.id}`);
-		} else {
-			return CellOfType[column.type](cell, doCellChange, task, taskChange, ...options);
-		}
-	}
+  function createCell(
+    column = () => console.error('no column for cell'),
+    options = []
+  ) {
+    if (!column.type) throw new Error('No column.type for cell');
+    const cell = task.cells.find((cell) => cell.columnMatch === column._id);
+    if (!cell) {
+      console.error(`task with no cell match for column.id ${column.id}`);
+    } else {
+      return CellOfType[column.type](
+        cell,
+        doCellChange,
+        task,
+        taskChange,
+        ...options
+      );
+    }
+  }
 
-	async function cellChange(newTask) {
-		try {
-			// Change on server
-			let changedTask = await db_changeTask(newTask);
-			changedTask = changedTask.data;
-			/*	// TODO: This should check for the individual cell
+  async function cellChange(newTask) {
+    try {
+      // Change on server
+      let changedTask = await db_changeTask(newTask);
+      changedTask = changedTask.data;
+      /*	// TODO: This should check for the individual cell
 				console.error("Task on server does not match task change");
 			} */
 
-			// Change on store - only change the cell (not deepCloned)
+      // Change on store - only change the cell (not deepCloned)
 
-			/* editTask(groupIndex, taskIndex, task); */
-		} catch (error) {
-			console.error(error);
-		}
-	}
+      /* editTask(groupIndex, taskIndex, task); */
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-	function doLocalChange(cell) {
-		const newCells = _.cloneDeep(task.cells);
-		const cellIndex = newCells.findIndex((cellItem) => cellItem.columnMatch === cell.columnMatch);
-		if (cellIndex > -1) {
-			newCells[cellIndex] = cell;
-		}
-		const newTask = { ...task, cells: newCells };
-		editTask(groupIndex, taskIndex, newTask);
-		/* setTask(newTask); */
-		return newTask;
-	}
+  function doLocalChange(cell) {
+    const newCells = _.cloneDeep(task.cells);
+    const cellIndex = newCells.findIndex(
+      (cellItem) => cellItem.columnMatch === cell.columnMatch
+    );
+    if (cellIndex > -1) {
+      newCells[cellIndex] = cell;
+    }
+    const newTask = { ...task, cells: newCells };
+    editTask(groupIndex, taskIndex, newTask);
+    /* setTask(newTask); */
+    return newTask;
+  }
 
-	function doCellChange(cell, debounced) {
-		const delayedCellChange = _.debounce((newTask) => {
-			cellChange(newTask);
-		}, 800);
+  function doCellChange(cell, debounced) {
+    const delayedCellChange = _.debounce((newTask) => {
+      cellChange(newTask);
+    }, 800);
 
-		if (debounced) {
-			const newTask = doLocalChange(cell);
-			// cellChange(newTask);
-			/* delayedCellChange.cancel();
+    if (debounced) {
+      const newTask = doLocalChange(cell);
+      // cellChange(newTask);
+      /* delayedCellChange.cancel();
 			delayedCellChange(newTask); */
-		} else {
-			delayedCellChange.cancel();
-			const newTask = doLocalChange(cell);
-			cellChange(newTask);
-		}
-	}
+    } else {
+      delayedCellChange.cancel();
+      const newTask = doLocalChange(cell);
+      cellChange(newTask);
+    }
+  }
 
-	async function taskChange(changedTask) {
+  async function taskChange(changedTask) {
+    editTask(groupIndex, taskIndex, changedTask);
+    try {
+      let resChangedTask = await db_changeTask(changedTask);
+      resChangedTask = resChangedTask.data;
+      if (resChangedTask.isMock !== task.isMock) return;
+      editTask(groupIndex, taskIndex, resChangedTask);
+    } catch (error) {
+      // This changes task back to the task in the functions closure
+      editTask(groupIndex, taskIndex, task);
+      console.error(error);
+    }
+  }
 
-		editTask(groupIndex, taskIndex, changedTask);
-		try {
-			let resChangedTask = await db_changeTask(changedTask);
-			resChangedTask = resChangedTask.data;
-			editTask(groupIndex, taskIndex, resChangedTask);
-		} catch (error) {
-			// This changes task back to the task in the functions closure
-			editTask(groupIndex, taskIndex, task);
-			console.error(error);
-		}
-	}
+  function unMockTask() {
+    taskChange({ ...task, isMock: false });
+    addNewTask();
+  }
 
-	function unMockTask(){
-		taskChange({...task, isMock: false});
-		addNewTask();
-	}
-
-	return inView ? ( // TODO: maby add this to a list of viewed on group and render there accordingly (no need to pass anything)
-		<div className={style["task"]} ref={taskWrapperRef} 
-			style={task.isMock ? {opacity: "50%"} : {}}
-			onFocus={task.isMock && unMockTask}>
-			<SelectCheckbox 
-				taskIsSelected={taskIsSelected} 
-				setTaskIsSelected={setTaskIsSelected}
-				taskId={task._id} 
-				groupIndex={groupIndex}
-				/>
-			<div className={style["task-handle"]}>&#8942;</div>
-			{columns.map((column) => {
-				return (
-					<CellWrapper key={column.id} column={column}>
-						{createCell(column)}
-					</CellWrapper>
-				);
-			})}
-		</div>
-	) : (
-		<div key={"task.id"} className={style["pseudo-task"]} ref={taskWrapperRef}></div>
-	);
+  return inView ? ( // TODO: maby add this to a list of viewed on group and render there accordingly (no need to pass anything)
+    <div
+      className={style['task']}
+      ref={taskWrapperRef}
+      style={task.isMock ? { opacity: '50%' } : {}}
+      onFocus={task.isMock ? unMockTask : null}
+    >
+      {!task.isMock && (
+        <SelectCheckbox
+          taskIsSelected={taskIsSelected}
+          setTaskIsSelected={setTaskIsSelected}
+          taskId={task._id}
+          groupIndex={groupIndex}
+        />
+      )}
+      <div className={style['task-handle']}>&#8942;</div>
+      {columns.map((column) => {
+        return (
+          <CellWrapper key={column.id} column={column}>
+            {createCell(column)}
+          </CellWrapper>
+        );
+      })}
+    </div>
+  ) : (
+    <div
+      key={'task.id'}
+      className={style['pseudo-task']}
+      ref={taskWrapperRef}
+    ></div>
+  );
 }
 
 Task.propTypes = {};
